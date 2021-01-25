bc347f241fa543cb074627634202e40a678948bf
cdv <cdv@sfu.ca>
2021-01-24 23:49:51 -0800

    Initial Commit

---
+++ web/src/test/java/ca/cdv/mathwebsite/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.cdv.mathwebsite.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
